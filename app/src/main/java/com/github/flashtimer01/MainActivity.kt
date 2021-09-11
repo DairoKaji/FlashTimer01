4420ec03fd449a9ab3143ab53cc77f1213d96a45
Dairokaji <dakaji@technoblood.com>
2021-09-11 17:00:10 +0900

    ・敵のレベルを設定するボタンを追加 ・アイオニアブーツの有無を設定するボタンを追加

--- app/src/main/java/com/github/flashtimer01/MainActivity.kt
+++ app/src/main/java/com/github/flashtimer01/MainActivity.kt
@@ -1,16 +1,65 @@
 package com.github.flashtimer01
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.github.flashtimer01.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        button_flash.setOnClickListener {
-            val txt = time_flash.text
-            time_flash.text = "5:00"
+        var nowLevel :Int = 1
+        var hasCosmicInsight :Int = 0
+        var hasIonianBoots :Int = 0
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.buttonFlash.setOnClickListener {
+            val txt = binding.timeFlash.text
+            binding.timeFlash.text = "5:00"
         }
+        binding.buttonEnemyLevelPlus.setOnClickListener {
+            val num = enemyLevelCalc("plus",nowLevel)
+            val txt = binding.enemyLevelCount.text
+            binding.enemyLevelCount.text = num.toString()
+            nowLevel = num
+        }
+        binding.buttonEnemyLevelMinus.setOnClickListener {
+            val num = enemyLevelCalc("minus",nowLevel)
+            val txt = binding.enemyLevelCount.text
+            binding.enemyLevelCount.text = num.toString()
+            nowLevel = num
+        }
+        binding.buttonCosmicInsight.setOnClickListener {
+            if (hasCosmicInsight == 1) {
+                hasCosmicInsight = 0
+                val img = binding.buttonIonianBoots.setImageResource(R.drawable.ionia_boots_dark)
+            }else{
+                hasCosmicInsight = 1
+                val img = binding.buttonIonianBoots.setImageResource(R.drawable.ionia_boots)
+            }
 
+        }
+    }
+    private fun enemyLevelCalc(opSign : String,nowLevelFun:Int): Int {
+        var number:Int = nowLevelFun
+        if(opSign == "plus"){
+            if (number <=17) {
+                number += 1
+                return number
+            }else{
+                Toast.makeText(applicationContext , "Level Max", Toast.LENGTH_SHORT).show()
+                return number
+            }
+        }else{
+            if (number >=2) {
+                number -= 1
+                return number
+            }else {
+                Toast.makeText(applicationContext , "Level Min", Toast.LENGTH_SHORT).show()
+                return number
+            }
+        }
     }
 }
